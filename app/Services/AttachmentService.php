d14923e77091c4744fb6695df558b188c2317afa
ltstudy <ltstudy@sina.com>
2018-04-30 15:43:38 +0800

    推

--- app/Services/AttachmentService.php
+++ app/Services/AttachmentService.php
@@ -25,7 +25,8 @@ Class AttachmentService extends Service{
             if( !request()->hasFile('cam_file') ) {
                throw new Exception('文件不能空',2);
             }
-            $file = request()->file('cam_file','');
+
+            $file = request()->file('files','');
 
             /*上传文件路径*/
             $path = $file->store('attachments',$this->disk);
@@ -35,6 +36,7 @@ Class AttachmentService extends Service{
 
             /*文件信息*/
             $fileName = $file->hashName();
+
             $data = [
                 'name' => $fileName,
                 'origin_name' => $file-> getClientOriginalName(),
@@ -42,23 +44,30 @@ Class AttachmentService extends Service{
                 'path' => $path,
                 'ext' => $file->getClientOriginalExtension(),
                 'ext_info' => '',
-                'supply_id' => '',
+                'status' => request()->status,
+                'user_id' => $user->id,
             ];
+
             if( $attachment = $this->attachmentRepo->create($data) ){
 
-                  $this->results = array_merge($this->results,[
+                $this->results = array_merge($this->results,[
+
                     'id' => $attachment->id,
-                    'name' => $attachment->name,
+                    'name' => str_replace('.' .$attachment->ext ,'',$attachment->origin_name),
                     'ext' => $attachment->ext,
                     'size' => $attachment->size,
-                    'url' => '',
-                    'created_at' => $this->created_at->formate('Y-m-d H:i:s'),
-                ]);
+                    'url' => route('api.attachement.cam.list', [$attachment->id]),
+                    'created_at' => $attachment->created_at->format('Y-m-d H:i:s'),
+
+                  ]);
 
             } else {
                 throw new EXception('附件上传失败',2);
             }
-            return array_merge($this->results,['code' => 200]);
+
+            return array_merge($this->results,[
+                'code' => 200
+            ]);
 
         });
 
@@ -72,7 +81,51 @@ Class AttachmentService extends Service{
      */
     public function uploadList($id)
     {
-        /*检测文件是否存在*/
+        try{
+            /*检测文件是否存在*/
+            $attachments = $this->attachmentRepo->find($id);
+
+            if( Storage::disk($this->disk)->exists($attachments->path) ) {
+                /* 获取文件 */
+                $path =  Storage::disk($this->disk)->exists($attachments->path);
+                $name = $attachments->origin_name;
+
+                return [
+                    'path' => $path,
+                    'name' => $name,
+                ];
+
+            } else {
+                abort(404,'文件不存在');
+            }
+
+        } catch(Exception $e) {
+            abort(404,'文件不存在');
+
+        }
+    }
+
+    /**
+     * 用户头像
+     * return [type] [description]
+     */
+    public function avatar($id)
+    {
+        try{
+            /*查看当前用户信息*/
+            $user = $this->userRepo->find($id);
+            if( Storage::disk($this->disk)->exists($user->avatar) ) {
+
+               $path =  Storage::disk($this->disk)->exists($user->avatar);
+
+               return $path;
+            } else {
+                abort(404,'没有头像');
+            }
+        } catch(Exception $e){
+            abort(404,'用户没有头像');
+        }
+
     }
 
 }
